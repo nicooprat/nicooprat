4e3898ea2977957be0a35492082fa4f10d5ce945
Nico Prat <nicooprat@gmail.com>
2022-09-21 21:53:40 +0200

    Full static generate

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,7 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
+  target: 'static',
+  ssr: false,
   modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt'],
   runtimeConfig: {
     apifyToken: process.env.APIFY_TOKEN,
